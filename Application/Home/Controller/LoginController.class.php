c765af7a9f3487ae50531fc0a4de73e1ad6156d5
sunjianjian <a1980439463@163.com>
2018-02-07 14:31:03 +0800

    shejuyi

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -195,18 +195,18 @@
         {
             $verify = new Geetest(C('CAPTCHA_ID'),C("PRIVATE_KEY"));
 
-            if(!isset($_SESSION['geetest_user_id'])){
-              $_SESSION['geetest_user_id']=uniqid();// 生成一个唯一ID
+            if(!isset($_SESSION['user_id'])){
+              $_SESSION['user_id']=uniqid();// 生成一个唯一ID
             }
             $data = array(
-                "geetest_user_id" => $_SESSION['geetest_user_id'], # 网站用户id
+                "user_id" => $_SESSION['user_id'], # 网站用户id
                 "client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
-                "ip_address" => "127.0.0.1" # 请在此处传输用户请求验证时所携带的IP
+                "ip_address" => $_SERVER['REMOTE_ADDR'] # 请在此处传输用户请求验证时所携带的IP
             );
 
             $status = $verify->pre_process($data, 1);
             $_SESSION['gtserver'] = $status;
-            $_SESSION['geetest_user_id'] = $data['geetest_user_id'];
+            $_SESSION['user_id'] = $data['user_id'];
             echo $verify->get_response_str();
         }
         //二次验证
@@ -218,18 +218,23 @@
   
             // 比如你设置了一个验证码是否验证通过的标识
             $code_flag=false;
+
+            $data = array(
+                "user_id" => $_SESSION['user_id'], # 网站用户id
+                "client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
+                "ip_address" => $_SERVER['REMOTE_ADDR'] # 请在此处传输用户请求验证时所携带的IP
+            );
               
             // 这里获取你之前设置的user_id，传送给极验服务器做校验
-            $user_id = $_SESSION['geetest_user_id'];
+            
             if ($_SESSION['gtserver'] == 1) {
-                $result = $verify->success_validate($geetest_challenge, $geetest_validate, $geetest_seccode, $user_id);
+                $result = $verify->success_validate($geetest_challenge, $geetest_validate, $geetest_seccode, $data);
+                
                 if ($result) {
-                    // 验证码验证成功
                     $code_flag=true;
                }
             }else{
                if ($verify->fail_validate($geetest_challenge,$geetest_validate,$geetest_seccode)) {
-                    // 验证码验证成功
                     $code_flag=true;
               }
             }
