50814ee7a34fd29c91ca72f8aed2a5c5c63a9f41
sunjianjian <a1980439463@163.com>
2018-03-27 00:01:25 +0800

    shejuyi

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -1,11 +1,44 @@
 <?php
 	namespace Home\Controller;
+	Vendor('qiniu.autoload');  //七牛入口文件引入 
 	use Think\Controller;
+	// 引入鉴权类
+	use Qiniu\Auth;
+	// 引入上传类
+	use Qiniu\Storage\UploadManager;
 	class TestController extends Controller
 	{
        public function index()
        {
-       		$this->display();
+       		// 需要填写你的 Access Key 和 Secret Key
+			$accessKey = "wHEzFcA6lp2GKlVaCi_aaR2TLr4Vkqg6UhJvMgsG";
+			$secretKey = "Yy2yJPJ3r8Ze6wXZoa-MjtbIwV8nDUwBbtRqPHZV";
+			$bucket = "xiaoheiwu";
+
+			// 构建鉴权对象
+			$auth = new Auth($accessKey, $secretKey);
+
+			// 生成上传 Token
+			$token = $auth->uploadToken($bucket);
+
+			// 要上传文件的本地路径
+			$filePath = './moren.png';
+
+			// 上传到七牛后保存的文件名 
+			$key = 'my/php/logo.png';
+
+			// 初始化 UploadManager 对象并进行文件的上传。
+			$uploadMgr = new UploadManager();
+
+			// 调用 UploadManager 的 putFile 方法进行文件的上传。
+			list($ret, $err) = $uploadMgr->putFile($token, $key, $filePath);
+			echo "\n====> putFile result: \n";
+			if ($err !== null) {
+				var_dump($err);
+			} else {
+				var_dump($ret);
+			}
+
        } 
        public function haha()
        {
